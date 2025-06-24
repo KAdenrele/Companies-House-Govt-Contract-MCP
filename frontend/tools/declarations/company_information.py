ca7c7b75b02750946b541c6ec5c4da0783270286
KAdenrele <akomolafe@zaizi.com>
2025-06-24 17:46:49 +0100

    smal error in company information decalration resolved

--- frontend/tools/declarations/company_information.py
+++ frontend/tools/declarations/company_information.py
@@ -4,7 +4,7 @@ from google.generativeai.types import FunctionDeclaration
 get_company_profile_declaration = FunctionDeclaration(
     name="get_company_profile", 
     #the second line here might be crucial for the tool to be called without the companyNumber which then triggers the condition for providing the list.
-    description="Provide a company profile from companies house." #If the company number is unknown, call the tool without it to get a list of suggestions.",
+    description="Provide a company profile from companies house.",# #If the company number is unknown, call the tool without it to get a list of suggestions.",
     parameters={
         "type": "OBJECT",
         "properties": {
